e9b1d26578784eadb9b32da47318ddf7ff081d2d
DP1KEsmits <>
2021-06-01 11:32:44 +0000

    Izmainitas failu mapes

--- main.py
+++ main.py
@@ -7,9 +7,20 @@ def index():
   return render_template("index.html")
 
 
-@app.route('/about')
-def about():
-  return render_template("about.html")
+@app.route('/admin')
+def admin():
+  return render_template("admin.html")
 
+@app.route('/izvele')
+def izvele():
+  return render_template("izvele.html")
+
+@app.route('/rezervacijas')
+def rezervacijas():
+  return render_template("rezervacijas.html")
+
+@app.route('/statistika')
+def statistika():
+  return render_template("statistika.html")
 
 app.run(host='0.0.0.0', port=8080)
