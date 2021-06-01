f8ff70f2e20f1d2fe28be60443186c9786cc38e3
Kristaps2004 <78017341+Kristaps2004@users.noreply.github.com>
2021-06-01 14:34:05 +0300

    Add files via upload

---
+++ static/js/sakums.js
@@ -0,0 +1,9 @@
+document.getElementById("login").addEventListener("click", () => {
+  let popUp = document.getElementById("popUp");
+  popUp.style.display = "block";
+});
+
+document.getElementById("nakamais").addEventListener("click", () => {
+  let popUp1 = document.getElementById("popUp1");
+  popUp1.style.display = "block";
+});
