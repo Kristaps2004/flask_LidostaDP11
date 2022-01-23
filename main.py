b8517ecbf2850dd3d05e89a0f558d566da8cae5f
DP1KEsmits <78017341+Kristaps2004@users.noreply.github.com>
2022-01-23 21:29:40 +0000

    Izveidoju nederīgu datbu bāzi whooo yeee

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-app = Flask('app')
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+  id = db.Column(db.Integer, primary_key = True)
+  content = db.Column(db.String(200), nullable = False)
+  date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+  def __repr__(self):
+    return 'Task %r' %self.id
 
 @app.route('/')
 def index():
   return render_template("index.html")
-
 
 @app.route('/admin')
 def admin():
@@ -37,5 +47,5 @@ def lidmasinas():
 def reisi():
   return render_template("adminreisi.html")
 
-
-app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__": 
+  app.run(host='0.0.0.0', port=8000)
